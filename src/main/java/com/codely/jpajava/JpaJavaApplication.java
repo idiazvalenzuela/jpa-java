bdb2bd108c3befbb56a23c7dbc7be8c765746706
Irene Díaz Valenzuela <irene.diazvalenzuela@manomano.com>
2022-10-09 16:52:28 +0200

    fix: move annotation to proper place

--- src/main/java/com/codely/jpajava/JpaJavaApplication.java
+++ src/main/java/com/codely/jpajava/JpaJavaApplication.java
@@ -2,8 +2,10 @@ package com.codely.jpajava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class JpaJavaApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/codely/jpajava/application/repository/AnimalRepository.java
+++ src/main/java/com/codely/jpajava/application/repository/AnimalRepository.java
@@ -1,12 +1,11 @@
 package com.codely.jpajava.application.repository;
 
 import com.codely.jpajava.domain.Animal;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-@EnableJpaRepositories
+
 public interface AnimalRepository extends CrudRepository<Animal, Integer> {
 
     Animal findAnimalByName(String name);
